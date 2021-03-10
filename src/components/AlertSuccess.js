8b1700f9e6e29c46c188cd949cc97f58a08291a7
priyam1103 <priyampoddar89@gmail.com>
2021-03-10 15:55:42 +0530

    first commit

--- src/components/AlertSuccess.js
+++ src/components/AlertSuccess.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Message } from "semantic-ui-react";
+import {GlobalContext} from "../context/GlobalState"
 export default function AlertSuccess({ message }) {
+    const { removeAlert } = useContext(GlobalContext);
   return (
     <div className="alert">
-      <Message positive color="green">
+      <Message positive color="green"  onDismiss={removeAlert}>
         <Message.Header>Success !</Message.Header>
         <p>{message}</p>
       </Message>
